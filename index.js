24dd408d97a307da806c75510761e0d40a10c0f3
Tuuli Sarantola <tuuli.sarantola@gmail.com>
2018-11-29 22:59:07 +0200

    Excercises 3.7 and 3.8

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
+const morgan = require('morgan')
 
 app.use(bodyParser.json())
+morgan.token('body', function (req, res) { return JSON.stringify(req.body) })
+app.use(morgan(':method :url :body :status :res[content-length] - :response-time ms'))
 
 let persons = [
   {
